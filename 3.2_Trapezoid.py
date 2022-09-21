fade3b1971cbb47f2e40580b3848edfb0c6cf35a
linthicumrac <linthicumrac@urbandaleschools.com>
2022-09-20 21:10:39 -0500

    Jedi_Training Commit

--- 3.2_Trapezoid.py
+++ 3.2_Trapezoid.py
@@ -1,7 +1,8 @@
-'''
+"""
 TRAPEZOID PROGRAM
 -------------------
-Create a new program that will ask the user for the information needed to find the area of a trapezoid, and then print the area.
+Create a new program that will ask the user for the information needed to find the area of a trapezoid
+then print the area.
 
 Test with the following:
 
@@ -9,7 +10,7 @@ base 1: 2       base 2: 3    height: 4    area: 10
 base 1: 5       base 2: 7    height: 2    area: 12
 base 1: 1       base 2: 2    height: 3    area: 4.5
 base 1: 7       base 2: 2    height: 4    area: 18
-'''
+"""
 base = float(input("What is the base of your triangle in in?"))
 b2 = float(input("what is the second base of the trapezoid in in?"))
 height = float(input("What is the height of your triangle in in?"))
